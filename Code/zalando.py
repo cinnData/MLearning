10a94ab911c86cc9a032cdea3be7bc35375582d9
cinnData <canela.miguel@icloud.com>
2022-03-18 13:21:08 +0100

    Update zalando.py

--- Code/zalando.py
+++ Code/zalando.py
@@ -50,7 +50,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/7, random_
 
 # Random forest classifier #
 from sklearn.ensemble import RandomForestClassifier
-rfclf = RandomForestClassifier(max_leaf_nodes=128, n_estimators=100)
+rfclf = RandomForestClassifier(max_leaf_nodes=256, n_estimators=100)
 rfclf.fit(X_train, y_train)
 round(rfclf.score(X_train, y_train), 3), round(rfclf.score(X_test, y_test), 3)
 
