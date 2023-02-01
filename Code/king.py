96a363b6a47d23fb649dd85f103bf5ca74fb602d
cinnData <canela.miguel@icloud.com>
2023-02-01 19:37:48 +0100

    Update king.py

--- Code/king.py
+++ Code/king.py
@@ -1,66 +1,46 @@
 ## Example - House sales in King County ##
 
-# Importing the data #
-import numpy as np
-path = 'https://raw.githubusercontent.com/cinnData/MLearning/main/Data/'
-fname = path + 'king.csv'
-data = np.genfromtxt(fname, delimiter=',', names=True, dtype=None, encoding='utf-8')
-data.shape
-data[:2]
+# Importing the data (edit path) #
+import csv
+with open('Dropbox/ml_course/data/king.csv', 'r') as conn:
+	reader = csv.reader(conn)
+	data = list(reader)
+len(data)
+
+# Headers #
+header = data[0]
+header
+len(header)
 
 # Target vector and feature matrix #
-y = data['price']/1000
-X = data[list(data.dtype.names[3:-1])]
-from numpy.lib.recfunctions import structured_to_unstructured
-X = structured_to_unstructured(X)
+import numpy as np
+Xy = np.array(data[1:])
+Xy.shape
+y = Xy[:, 15].astype(float)/1000
+y.shape
+X = Xy[:, 3:15].astype(float)
 X.shape
 
-# The distribution of the sale price #
+# Q1. Distribution of the sale price #
 from matplotlib import pyplot as plt
 plt.figure(figsize=(8,6))
 plt.title('Figure 1. Sale price')
 plt.hist(y, color='gray', rwidth=0.97)
 plt.xlabel('Sale price (thousands)');
 
-# # Linear regression (first round) #
+# Q2. Linear regression model #
 from sklearn.linear_model import LinearRegression
-linreg1 = LinearRegression()
-linreg1.fit(X, y)
-round(linreg1.score(X, y), 3)
-ypred1 = linreg1.predict(X)
+linreg = LinearRegression()
+linreg.fit(X, y)
+round(linreg.score(X, y), 3)
+y_pred = linreg.predict(X)
+np.corrcoef(y, y_pred)
+np.corrcoef(y, y_pred)[0, 1]**2
+
+# Q3. Actual price versus predicted price #
 plt.figure(figsize=(6,6))
-plt.scatter(x=ypred1, y=y, color='black', s=1)
+plt.scatter(x=y_pred, y=y, color='black', s=1)
 plt.title('Figure 2. Actual vs predicted price')
 plt.xlabel('Predicted price (thousands)')
 plt.ylabel('Actual price (thousands)');
-np.sum(ypred1 < 0)
-
-# Incorporating the zipcode #
-X1 = data[list(data.dtype.names[5:-1])]
-X1 = structured_to_unstructured(X1)
-from sklearn.preprocessing import OneHotEncoder
-enc = OneHotEncoder()
-enc.fit(data['zipcode'].reshape(21613,1))
-X2 = enc.transform(data['zipcode'].reshape(21613,1)).toarray()
-X2.shape
-np.unique(X2, return_counts=True)
-X = np.concatenate([X1, X2], axis=1)
-X.shape
-
-# Linear regression (second round) #
-linreg2 = LinearRegression()
-linreg2.fit(X, y)
-round(linreg2.score(X, y), 3)
-ypred2 = linreg2.predict(X)
-plt.figure(figsize=(6,6))
-plt.scatter(x=ypred2, y=y, color='black', s=1)
-plt.title('Figure 3. Actual vs predicted price (2nd round)')
-plt.xlabel('Predicted price (thousands)')
-plt.ylabel('Actual price (thousands)');
-np.sum(ypred2 < 0)
-
-
-
-
-
-
+np.sum(y_pred < 0)
