428192523c172f4692e0d72791a8634f3b7e347a
cinnData <canela.miguel@icloud.com>
2023-02-19 19:47:42 +0100

    Update spam.py

--- Code/spam.py
+++ Code/spam.py
@@ -1,69 +1,78 @@
 ## Example - The spam filter ##
 
 # Importing the data #
+import csv
 import numpy as np
-path = 'https://raw.githubusercontent.com/cinnData/MLearning/main/Data/'
-fname = path + 'spam.csv'
-data = np.genfromtxt(fname, delimiter=',', names=True, dtype=None, encoding='utf-8')
-data.shape
-data[:1]
-round(np.mean(data['spam']), 3)
+with open('Dropbox/ml_course/data/spam.csv', mode='r') as conn:
+	reader = csv.reader(conn)
+	data = list(reader)
+len(data)
+
+# Headers #
+header = data[0]
+header
+len(header)
 
 # Target vector and feature matrix #
-y = data['spam']
-from numpy.lib.recfunctions import structured_to_unstructured
-X = data[list(data.dtype.names)[:-1]]
-X = structured_to_unstructured(X)
+import numpy as np
+Xy = np.array(data[1:])
+Xy.shape
+y = Xy[:, 51].astype(float)
+y.shape
+X = Xy[:, :51].astype(float)
 X.shape
 
-# Decision tree classifier #
-from sklearn.tree import DecisionTreeClassifier
-treeclf1 = DecisionTreeClassifier(max_depth=2)
-treeclf1.fit(X, y)
-round(treeclf1.score(X, y), 3)
+# Spam rate #
+y.mean().round(3)
 
-# Confusion matrix #
-ypred1 = treeclf1.predict(X)
+# Q1a1. Decision tree classifier (max depth = 2) #
+from sklearn.tree import DecisionTreeClassifier
+clf1 = DecisionTreeClassifier(max_depth=2)
+clf1.fit(X, y)
+round(clf1.score(X, y), 3)
+
+# Q1a2. Confusion matrix #
+y_pred1 = clf1.predict(X)
 from sklearn.metrics import confusion_matrix
-conf1 = confusion_matrix(y, ypred1)
+conf1 = confusion_matrix(y, y_pred1)
 conf1
 tp1 = conf1[1, 1]/np.sum(conf1[1, :])
 fp1 = conf1[0, 1]/np.sum(conf1[0, :])
 round(tp1, 3), round(fp1, 3)
 
-# Deeper tree #
-treeclf2 = DecisionTreeClassifier(max_depth=3)
-treeclf2.fit(X, y)
-ypred2 = treeclf2.predict(X)
-conf2 = confusion_matrix(y, ypred2)
+# Q1b. Decision tree classifier (max depth = 3) #
+clf2 = DecisionTreeClassifier(max_depth=3)
+clf2.fit(X, y)
+y_pred2 = clf2.predict(X)
+conf2 = confusion_matrix(y, y_pred2)
 conf2
 tp2 = conf2[1, 1]/np.sum(conf2[1, :])
 fp2 = conf2[0, 1]/np.sum(conf2[0, :])
 round(tp2, 3), round(fp2, 3)
 
-# Deeper tree #
-treeclf3 = DecisionTreeClassifier(max_depth=4)
-treeclf3.fit(X, y)
-ypred3 = treeclf3.predict(X)
-conf3 = confusion_matrix(y, ypred3)
+# Q1c. Decision tree classifier (max depth = 4) #
+clf3 = DecisionTreeClassifier(max_depth=4)
+clf3.fit(X, y)
+y_pred3 = clf3.predict(X)
+conf3 = confusion_matrix(y, y_pred3)
 conf3
 tp3 = conf3[1, 1]/np.sum(conf3[1, :])
 fp3 = conf3[0, 1]/np.sum(conf3[0, :])
 round(tp3, 3), round(fp3, 3)
 
-# Deeper tree #
-treeclf4 = DecisionTreeClassifier(max_depth=5)
-treeclf4.fit(X, y)
-ypred4 = treeclf4.predict(X)
-conf4 = confusion_matrix(y, ypred4)
+# Q1d. Decision tree classifier (max depth = 5) #
+clf4 = DecisionTreeClassifier(max_depth=5)
+clf4.fit(X, y)
+y_pred4 = clf4.predict(X)
+conf4 = confusion_matrix(y, y_pred4)
 conf4
 tp4 = conf4[1, 1]/np.sum(conf4[1, :])
 fp4 = conf4[0, 1]/np.sum(conf4[0, :])
 round(tp4, 3), round(fp4, 3)
 
-# Feature importance #
-imp = treeclf4.feature_importances_
-feat_list = np.array(data.dtype.names[:-1])[imp > 0]
-feat_imp = np.round(treeclf4.feature_importances_[imp > 0], 3)
+# Q2. Feature relevance #
+imp = clf4.feature_importances_
+feat_list = np.array(header[:-1])[imp > 0]
+feat_imp = np.round(clf4.feature_importances_[imp > 0], 3)
 feat_report = np.array([feat_list, feat_imp], dtype=object).transpose()
 feat_report[np.argsort(np.array(feat_report[:, 1], dtype='float'))[::-1], :]
